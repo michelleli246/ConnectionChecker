afc2aca7e0d6a13a54bf86f772acd6e7d3a05c54
MichLi123 <35055657+MichLi123@users.noreply.github.com>
2018-11-04 18:39:45 -0500

    screen on event works

--- app/src/main/java/li/michelle/connection_checker/MainActivity.java
+++ app/src/main/java/li/michelle/connection_checker/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -16,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final IntentFilter theFilter = new IntentFilter();
+        /** System Defined Broadcast */
+        theFilter.addAction(Intent.ACTION_SCREEN_ON);
+
+        getApplicationContext().registerReceiver(new StartActivityOnBoot(), theFilter);
+
+
         if(isNetworkConnected() == 1){
             setContentView(R.layout.wifi_on);
         }
--- app/src/main/java/li/michelle/connection_checker/StartActivityOnBoot.java
+++ app/src/main/java/li/michelle/connection_checker/StartActivityOnBoot.java
@@ -3,11 +3,14 @@ package li.michelle.connection_checker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class StartActivityOnBoot extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent){
+        String intentStr = intent.getAction();
+        Log.d("tag", intentStr);
         if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())){
             Intent i = new Intent(context, MainActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
